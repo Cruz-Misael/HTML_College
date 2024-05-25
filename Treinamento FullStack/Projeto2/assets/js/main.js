30575cd18eeae7fc2f63ccaf162d853918274fab
Misael Cruz <116309889+Cruz-Misael@users.noreply.github.com>
2024-05-25 13:02:54 -0300

    Add files via upload

--- Treinamento FullStack/Projeto2/assets/js/main.js
+++ Treinamento FullStack/Projeto2/assets/js/main.js
@@ -4,58 +4,72 @@ function meuEscopo (){
     
     
     const form = document.querySelector(".form");
-    const resultado = document.querySelector(".resultado");
     
     form.addEventListener('submit', function (e){
 
         e.preventDefault();
 
-        console.log("Evento prevenido");
+        const inputPeso = e.target.querySelector('#peso');
+        const inputAltura = e.target.querySelector('#altura');
 
-        const peso = form.querySelector('.peso');
-        const altura = form.querySelector('.altura');
+        peso = Number(inputPeso);
+        altura = Number(inputAltura);
 
-        function calculadora (peso, altura){
+        if (!peso) {
+            setResultado('Peso inválido', false);
+            return;
+          }
+        
+        if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+        }
 
-            let imc = "";
+        const imc = getImc(peso, altura);
+        const nivelImc = getNivelImc(imc);
 
-            if ( altura != null){
-                resultado.innerHTML = `<p> altura inválida </p>`;
-            }
-            else if (peso != null){
-                resultado.innerHTML = `<p> peso inválida </p>`;
-            }
-            else{
-                valor = peso/altura*altura;
-                if (valor < 18.5){
-                    imc = "Abaixo do peso";
-                }
-                else if(valor >= 18.5 && valor < 24.9){
-                    imc = "Peso normal";
-                }            
-                else if(valor >= 25 && valor < 29.9){
-                    imc = "Sobrepeso";
-                }                
-                else if(valor >= 30 && valor < 34.9){
-                    imc = "Obresidade grau 1";
-                }                
-                else if(valor >= 35 && valor < 39.9){
-                    imc = "Obresidade grau 2";
-                }                
-                else if(valor >= 40){
-                    imc = "Obresidade grau 3";
-                };             
-            }
-            
-            resultado.innerHTML += `<p> Seu IMC é ${valor} (${imc}) </p>`;
-                    
+        const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+        setResultado(msg, true);
+
+        function getNivelImc (imc) {
+            const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+              'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+          
+            if (imc >= 39.9) return nivel[5];
+            if (imc >= 34.9) return nivel[4];
+            if (imc >= 29.9) return nivel[3];
+            if (imc >= 24.9) return nivel[2];
+            if (imc >= 18.5) return nivel[1];
+            if (imc < 18.5) return nivel[0];
+          }
+          
+        function getImc (peso, altura) {
+        const imc = peso / altura ** 2;
+        return imc.toFixed(2);
+        }
+
+        function setResultado (msg, isValid) {
+        const resultado = document.querySelector('#resultado');
+        resultado.innerHTML = '';
+        
+        const p = criaP();
+        
+        if (isValid) {
+            p.classList.add('paragrafo-resultado');
+        } else {
+            p.classList.add('bad');
         }
         
-        calculadora (peso.value, altura.value);
-    });
+        p.innerHTML = msg;
+        resultado.appendChild(p);
+        }
+
+
 
         
-    }
+    });
+}
 
 
 meuEscopo();
